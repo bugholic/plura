c3010c6582baf60a849e9302752558062b05857a
bugholic <masterav2001@gmail.com>
2025-02-02 22:28:55 +0530

    add pr

---
+++ src/app/(main)/(agency)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import '@/app/globals.css'
+
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className="h-full flex items-center justify-center">{children}</div>;
+};
+
+export default AuthLayout;
---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { authMiddleware } from "@clerk/nextjs/server";
+
+export default authMiddleware({
+  publicRoutes: ["/site", "/api/uploadthing"],
+});
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+  ],
+};
