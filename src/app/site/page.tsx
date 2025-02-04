6225431432500b3d3b1b3683515be24b0f94f1b9
ayushv2001 <ayushv2001@gmail.com>
2025-02-04 11:55:44 +0000

    added components

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,14 +1,18 @@
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { pricingCards } from "@/lib/utils";
+import clsx from "clsx";
+import { Check } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
-      <section className="bg-black h-full w-full pt-36 relative flex items-center justify-center flex-col">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#161616,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
-        <p className="text-center mt-[700px]">Run your agency, in one place</p>
-        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text">
-          <h1 className="text-9xl font-bold text-center md:text-[300px] ">
+      <section className="md:mt-[300px] bg-black h-full w-full pt-36 relative flex items-center justify-center flex-col">
+        <div className=" absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#161616,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)]" />
+        <p className="text-center">Run your agency, in one place</p>
+        <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text ">
+          <h1 className="text-9xl font-bold text-center md:text-[300px]">
             Plura
           </h1>
         </div>
@@ -23,15 +27,32 @@ export default async function Home() {
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
-      <section className="flex justify-center flex-col gap-4 mt-20">
+      <section className="flex justify-center flex-col gap-4 md:mt-[300px] items-center">
         <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted">
           Our straight pricing plans are tailored to meet your needs. If{" "}
           {"you're"} not <br /> ready to commit you can get started for free.
         </p>
-        <div className="flex items-center gap-4 flex-wrap mt-6">
-          {pricingCards.map(card => (
-            <div key={card.title}></div>
+        <div className="flex  gap-4 flex-wrap mt-6">
+          {pricingCards.map((card) => (
+            <Card key={card.title} className={clsx('w-[300px] flex flex-col justify-between', { "border-2  border-blue-400": card.title === "Unlimited Saas", })}>
+              <CardHeader>
+                <CardTitle className={clsx('', { 'text-muted-[white]': card.title !== 'Unlimited Saas' })}>{card.title}</CardTitle>
+                <CardDescription>
+                  {card.description}
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <span className="text-4xl font-bold">{card.price}</span>
+                <span className="text-muted-foreground">/m</span>
+              </CardContent>
+              <CardFooter className="flex flex-col items-start gap-4 ">
+                <div>{card.features.map((feature) => <div key={feature} className="flex gap-2 items-center"><Check /> <p>{feature}</p> </div>)}
+                </div>
+                <Link href={`/agency?plan=${card.priceId}`} className={clsx('w-full text-center bg-[royalblue] p-2 rounded-md', { '!bg-blue-400': card.title === 'Unlimited Saas' })}>Get Started</Link>
+              </CardFooter>
+
+            </Card>
           ))}
         </div>
       </section>
